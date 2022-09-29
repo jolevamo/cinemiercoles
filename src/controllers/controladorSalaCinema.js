cb1ad034043a165c8050910dc8251d458e0739c1
505 <505@academicos.cesde.edu.co>
2022-09-28 20:44:20 -0500

    Pintar sillas

---
+++ src/controllers/controladorSalaCinema.js
@@ -0,0 +1,40 @@
+import {pintarSillas} from "../helper/pintarSillas.js"
+
+let asientos =[
+    [{id:"a1",estado:2},{id:"b1",estado:0},{id:"c1",estado:0},{id:"d1",estado:0}],
+    [{id:"a2",estado:0},{id:"b2",estado:0},{id:"c2",estado:0},{id:"d2",estado:0}],
+    [{id:"a3",estado:0},{id:"b3",estado:0},{id:"c3",estado:2},{id:"d3",estado:0}],
+]
+//Pintar desde js mi sala de cine:
+
+let cinema = document.getElementById("salacinema")
+
+//Recorreo los asientos y aplicar traversing
+
+pintarSillas(asientos, cinema)
+
+//Evento de clic en la sala de cine
+
+cinema.addEventListener("click",function(evento){
+    if (evento.target.tagName=="IMG"){
+        let idAsientoSeleccionado = evento.target.id
+        
+        asientos.forEach(function(hilera){
+            hilera.forEach(function(asiento){
+                if (asiento.id==idAsientoSeleccionado) {
+                    
+                    //encontre el asiento dónde el usuario se quiere sentar
+                    if (asiento.estado==0) {
+                        asiento.estado=1
+                        evento.target.src="../../assets/img/chairGreen.png"
+                    }else if(asiento.estado==1){
+                        asiento.estado=0
+                        evento.target.src="../../assets/img/chair.png"
+                    }
+                }
+
+            })
+        })
+    }
+})
+
